ead341fd545324888828d4bb42459bc73d486130
Arbaaz <arbaaz8158@gmail.com>
2026-08-29 14:56:57 +0530

    Binary Search function for decreasing order array.

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,23 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int BinarySearch(int arr[], int n, int key){
+// int BinarySearch(int arr[], int n, int key){
+//     int start = 0, end = n - 1, mid;
+
+//     while(start <= end){
+//         // Find mid
+//         mid = start + (end - start) / 2;
+
+//         // arr[mid] == key
+//         if(arr[mid] == key){
+//             return mid;
+//         }
+//         // arr[mid] < key
+//         else if(arr[mid] < key){
+//             start = mid + 1;
+//         }
+//         // arr[mid] > key
+//         else{
+//             end = mid - 1;
+//         }
+//     }
+//     return -1;
+// }
+
+// Binary Search in decreasing order array
+int BinarySearch(int arr[], int n, int key) {
     int start = 0, end = n - 1, mid;
 
-    while(start <= end){
+    while (start <= end) {
         // Find mid
         mid = start + (end - start) / 2;
 
         // arr[mid] == key
-        if(arr[mid] == key){
+        if (arr[mid] == key) {
             return mid;
         }
-        // arr[mid] < key
-        else if(arr[mid] < key){
+        // arr[mid] > key
+        else if (arr[mid] > key) {
             start = mid + 1;
         }
-        // arr[mid] > key
-        else{
+        // arr[mid] < key
+        else {
             end = mid - 1;
         }
     }
